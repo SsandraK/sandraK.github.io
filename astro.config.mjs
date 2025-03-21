4de3914feca77f9771008c1b17ae7c6af150c53f
Sandra Solovjov <sandrasolovjov@Sandras-MacBook-Pro-2.local>
2025-03-21 13:10:37 +0200

    github

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+const LIVE_URL = 'https://sandrak.github.io';
+
 export default defineConfig({
+  site: LIVE_URL,
+  base: '/',
   integrations: [tailwind()],
 });
